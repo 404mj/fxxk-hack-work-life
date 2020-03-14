a4d64af77e03dd0d975a7caad7dacdb2b88b5a7e
404mj <zhangshuxinmj@163.com>
2020-03-14 17:37:23 +0800

    add new scripts

---
+++ js/crack_wjx.js
@@ -0,0 +1,5 @@
+// https://ks.wjx.top/jq/39560621.aspx
+// 用js全部选中提交，查看答案。
+// https://www.html.cn/jqapi-1.9/category/selectors/attribute-selectors/
+
+$("a[rel='q1_1']").classList.add("jqChecked","jqChecked")
