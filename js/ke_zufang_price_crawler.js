39d95eae35661837a3f6c8b536146be64d3aa3b1
404mj <zhangshuxinmj@163.com>
2020-10-27 15:10:23 +0800

    compute median of prices

--- js/ke_zufang_price_crawler.js
+++ js/ke_zufang_price_crawler.js
@@ -7,7 +7,8 @@ sum=Number(0);
 elePathPre='//*[@id="content"]/div[1]/div[1]/div[';
 elePathSuff=']/div/span/em';
 
-for(var i=1;i<=15;++i){
+//计算租金均值
+for(var i=1;i<=num;++i){
     var elePath=elePathPre+i+elePathSuff;
     price=$x(elePath)[0].textContent;
 //    console.log(price);
@@ -15,7 +16,19 @@ for(var i=1;i<=15;++i){
 }
 console.log(sum/num);
 
-
-
-
+//计算租金中位数
+var priceList=new Array()
+for(var i=1;i<=num;++i) {
+    var elePath=elePathPre+i+elePathSuff;
+    price=$x(elePath)[0].textContent;
+    priceList.push(parseInt(price)); 
+}
+priceList.sort(function(a,b){
+    return a-b;
+});
+var half=Math.floor(num/2);
+if (num % 2)
+    console.log(priceList[half]);
+else
+    console.log((priceList[half - 1] + priceList[half]) / 2.0);
 
