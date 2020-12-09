bfd204afd90e604885f2743509b68caac6bc83e7
404mj <zhangshuxinmj@163.com>
2020-12-09 09:19:56 +0800

    new pics process,to get name and cut 200 a dir to satisfiy faceupload

--- python/ai_temp_device_pic.py
+++ python/ai_temp_device_pic.py
@@ -22,7 +22,7 @@ for path,dir_list,file_list in nowdir:
             insht.cell(start_row,5).value=ss[1]
             os.rename(file_name,ss[1]+picext)
             start_row+=1
-# 必须空一行xiongdei
+# 必须空一行
 inxls.save("6Lpics.xlsx")
 
 
@@ -43,4 +43,92 @@ for file in files:
     portion=os.path.splitext(file)
     if portion[1]==".JPG":
         os.rename(file,portion[0]+".jpg")
+
+#-------20201207 updated----------
+import os
+from PIL import Image
+
+for _,_,imgfile_list in os.walk('./'):
+    for imgname in imgfile_list:
+        if os.path.getsize(imgname) >= 300000:
+            img = Image.open(imgname)
+            img.save("compressed_"+imgname,
+             optimize=True,
+             quality=30)
+
+#--------20201207 updated 2---------
+pip3 install pillow optimize-images
+
+#----------处理文件名并写入excel------------
+import os
+import shutil
+from itertools import groupby
+import openpyxl
+
+i=0
+j=0
+dir=0
+xls='faceup'
+for path, pathlist, filelist in os.walk('./'):
+    for img in filelist:
+        phone=img.strip().split('-')[0]
+        name=img.strip.split('-')[1]
+        newname=phone+'.jpg'
+        os.rename(img,newname)
+        i+=1
+        if i%200 > 0:
+            xlsname=xls+str(j)+'.xlsx'
+            inxls=openpyxl.load_workbook("../faceup.xlsx")
+            insht=inxls.worksheets[0]
+            start_row=6 
+            insht.cell(start_row,1).value=name 
+            insht.cell(start_row,5).value=phone 
+            newdir=os.path.join(os.getcwd(),str(dir))
+            os.makedirs(newdir)
+            shutil.move(os.getcwd()+'/'+newname,newdir+'/'+newname)
+        else if :
+            pass
+# 无法实现自动命名且移动且写excel的逻辑。
+
+
+#--------------重新实现--------------
+import os
+import shutil
+from itertools import groupby
+import openpyxl
+
+#把文件名都读到二维list
+imgs=[]
+mid=[]
+i=0
+for path, pathlist, filelist in os.walk('./'):
+    for img in filelist:
+        mid.append(img)
+        i+=1
+        if i%2 == 0:
+            imgs.append(mid)
+            mid=[]
+    imgs.append(mid)
+
+for i in range(0,len(imgs)):
+    xlsname='faceup'+str(i)+'.xlsx'
+    subimg=imgs[i]
+    wb=openpyxl.Workbook()
+    ws=wb.active
+    startrow=1
+    newdir=os.path.join(os.getcwd(),str(i))
+    os.makedirs(newdir)
+    for j in range(0,len(subimg)):
+        img=subimg[j]
+        phone=img.strip().split('-')[0]
+        name=img.strip().split('-')[1].split('.')[0]
+        newname=phone+'.jpg'
+        os.rename(img,newname)
+        ws.cell(startrow,1).value=name
+        ws.cell(startrow,5).value=phone 
+        startrow+=1
+        shutil.move(os.getcwd()+'/'+newname,newdir+'/'+newname)
+    wb.save(xlsname)
+
+
 
